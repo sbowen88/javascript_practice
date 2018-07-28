b1e3f086f0a459397c485a1092a9f07d6cdb14e9
sbowen88 <sbowey@gmail.com>
2018-07-27 20:15:19 -0600

    response defined

--- asyncAndAwait.js
+++ asyncAndAwait.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
 
 async function fetchAvatar(userId){
-    const response = await fetch(``)
+    const response = await fetch(`https://catappapi.herokuapp.com/users/${userID}`)
+    
 }
