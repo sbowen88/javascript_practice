65cfda64a7ccc1f801897ec0e52be9c1b375ac3e
sbowen88 <sbowey@gmail.com>
2018-07-28 16:15:09 -0600

    data constant defined

--- asyncAndAwait.js
+++ asyncAndAwait.js
@@ -2,5 +2,5 @@ const fetch = require('node-fetch')
 
 async function fetchAvatar(userId){
     const response = await fetch(`https://catappapi.herokuapp.com/users/${userID}`)
-    
+    const data = await response.json()
 }
