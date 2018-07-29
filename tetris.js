1cb5bef033711c9cffd09e495bd07e0ff39632be
sbowen88 <sbowey@gmail.com>
2018-07-29 13:27:09 -0600

    board code

--- tetris.js
+++ tetris.js
@@ -9,3 +9,27 @@ var move = 0;
 var occupiedblocks = new Array();
 var direction = "";
 var points = 0;
+
+
+var board = document.getElementsByClassName('tetris-board')[0];
+board.innerHTML = '';
+var counter = 0;
+for (var y = 0; y < height; y++) {
+    var row = document.createElement('div');
+    row.className = 'row';
+    row.dataset.row = y;
+
+    for (var x = 0; x < width; x++) {
+        var block = document.createElement('div');
+        block.className = 'block';
+        block.dataset.x = x;
+        block.dataset.y = y;
+        block.dataset.index = counter;
+        block.dataset.state = 0;
+        block.innerHTML = "0 : " + counter;
+        row.appendChild(block);
+        counter++;
+    }
+
+    board.appendChild(row);
+}
