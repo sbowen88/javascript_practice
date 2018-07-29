2c3098bb103f16b9392b8293e2449b3f34bb2013
sbowen88 <sbowey@gmail.com>
2018-07-29 13:25:41 -0600

    variables defined

---
+++ tetris.js
@@ -0,0 +1,11 @@
+
+var shapes = new Array();
+var currentShape;
+var height = 15;
+var width = 10;
+var state = 1;      // 1 running - 0 paused - 2 game over
+var colors = ['black', 'orange', 'red', 'blue'];
+var move = 0;
+var occupiedblocks = new Array();
+var direction = "";
+var points = 0;
